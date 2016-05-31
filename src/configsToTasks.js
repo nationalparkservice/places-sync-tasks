54fae6a660ad3ceea0f18ce9ccdef1d854806fcb
Jim McAndrew <jim@loc8.us>
2016-05-31 09:01:10 -0600

    Write status back to task master db

--- src/configsToTasks.js
+++ src/configsToTasks.js
@@ -1,12 +1,20 @@
 var sync = require('places-sync');
 
-module.exports = function (configs) {
-  return configs.map(function (config) {
-    return {
+module.exports = function (configs, connection, updateSql) {
+  var returnArray = [];
+  configs.forEach(function (config) {
+    returnArray.push({
       'name': 'Sync ' + config.processName,
       'description': 'Syncing source: ' + config.source.name + ' to ' + config.destination.name,
       'task': sync,
       'params': [config.master, config.source, config.destination, config.twoWay]
-    };
+    });
+    returnArray.push({
+      'name': 'Update Status ' + config.processName,
+      'description': 'Updates the last_sync field',
+      'task': connection.query,
+      'params': [updateSql, config]
+    });
   });
+  return returnArray;
 };
