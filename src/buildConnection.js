804ca96dbd8bb59470069483d47447a4abc1bf69
Jim McAndrew <jim@loc8.us>
2016-06-06 13:57:13 -0400

    twoWay is now just one option

--- src/buildConnection.js
+++ src/buildConnection.js
@@ -6,15 +6,15 @@ module.exports = function (configList) {
     'master': {},
     'source': {},
     'destination': {},
-    'twoWay': false
+    'options': false
   };
 
   configList.forEach(function (config) {
     var newConfig = JSON.parse(JSON.stringify(configTemplate));
     var processName = config.process;
     newConfig.processName = processName;
-    newConfig.twoWay = config.two_way === true;
     try {
+      newConfig.options = config.options ? JSON.parse(config.options) : false;
       newConfig.source = readSource(config.source);
       newConfig.source.connection.processName = processName;
       newConfig.destination = readSource(config.destination);
