b44af3c32e2774b080f293d01c9655a247f275cd
Jim McAndrew <jim@loc8.us>
2016-06-28 11:38:28 -0600

    Rename fields / sync_sources ➠ places_sync_sources

--- sql/getTasks.sql
+++ sql/getTasks.sql
@@ -1,37 +1,37 @@
 SELECT
-  "process",
+  "name",
   "options",
-  "frequency",
+  "frequency_in_minutes",
   "last_sync",
   (SELECT row_to_json("_") FROM (
       SELECT
         *
       FROM
-        "sync_sources"
+        "places_sync_sources"
       WHERE
-        "sync_sources"."source_name" = "sync_tasks"."master"
+        "places_sync_sources"."source_name" = "sync_tasks"."master"
     ) AS "_"
   )::text AS "master",
   (SELECT row_to_json("_") FROM (
       SELECT
         *
       FROM
-        "sync_sources"
+        "places_sync_sources"
       WHERE
-        "sync_sources"."source_name" = "sync_tasks"."source"
+        "places_sync_sources"."source_name" = "sync_tasks"."source"
     ) AS "_"
   )::text AS "source",
   (SELECT row_to_json("_") FROM (
       SELECT
         *
       FROM
-        "sync_sources"
+        "places_sync_sources"
       WHERE
-        "sync_sources"."source_name" = "sync_tasks"."destination"
+        "places_sync_sources"."source_name" = "sync_tasks"."destination"
     ) AS "_"
   )::text AS "destination"
 FROM
-  "sync_tasks"
+  "places_sync_tasks"
 WHERE
-  (EXTRACT(EPOCH FROM NOW()-"last_sync")/3600 > "frequency" OR "last_sync" is null) AND
+  (EXTRACT(EPOCH FROM NOW()-"last_sync")/60 > "frequency" OR "last_sync" is null) AND
   "enabled" = true;
