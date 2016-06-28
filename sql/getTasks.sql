e0c9271f5cdd886a55274b03181b47890c756ebc
Jim McAndrew <jim@loc8.us>
2016-06-28 15:08:34 -0600

    Use the new source names, which are now numbers

--- sql/getTasks.sql
+++ sql/getTasks.sql
@@ -9,7 +9,7 @@ SELECT
       FROM
         "places_sync_sources"
       WHERE
-        "places_sync_sources"."source_name" = "sync_tasks"."master"
+        "places_sync_sources"."cartodb_id" = "places_sync_tasks"."master"
     ) AS "_"
   )::text AS "master",
   (SELECT row_to_json("_") FROM (
@@ -18,7 +18,7 @@ SELECT
       FROM
         "places_sync_sources"
       WHERE
-        "places_sync_sources"."source_name" = "sync_tasks"."source"
+        "places_sync_sources"."cartodb_id" = "places_sync_tasks"."source"
     ) AS "_"
   )::text AS "source",
   (SELECT row_to_json("_") FROM (
@@ -27,11 +27,11 @@ SELECT
       FROM
         "places_sync_sources"
       WHERE
-        "places_sync_sources"."source_name" = "sync_tasks"."destination"
+        "places_sync_sources"."cartodb_id" = "places_sync_tasks"."destination"
     ) AS "_"
   )::text AS "destination"
 FROM
   "places_sync_tasks"
 WHERE
-  (EXTRACT(EPOCH FROM NOW()-"last_sync")/60 > "frequency" OR "last_sync" is null) AND
+  (EXTRACT(EPOCH FROM NOW()-"last_sync")/60 > "frequency_in_minutes" OR "last_sync" is null) AND
   "enabled" = true;
