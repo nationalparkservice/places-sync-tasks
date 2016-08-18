2bec77f96471f96417d86e87e684e67579f05505
Jim McAndrew <jim@loc8.us>
2016-08-18 17:54:18 -0600

    Report the name of the task running

--- src/configsToTasks.js
+++ src/configsToTasks.js
@@ -4,6 +4,12 @@ module.exports = function (configs, connection, updateSql) {
   var returnArray = [];
   configs.forEach(function (config) {
     returnArray.push({
+      'name': 'Report Sync ' + config.processName,
+      'description': 'Syncing source: ' + config.source.name + ' to ' + config.destination.name,
+      'task': console.log,
+      'params': ['Syncing source: ' + config.source.name + ' to ' + config.destination.name]
+    });
+    returnArray.push({
       'name': 'Sync ' + config.processName,
       'description': 'Syncing source: ' + config.source.name + ' to ' + config.destination.name,
       'task': sync,
