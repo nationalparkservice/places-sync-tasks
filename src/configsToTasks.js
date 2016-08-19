b4ef873944f36728400abe648ed60131ed58a04a
Jim McAndrew <jim@loc8.us>
2016-08-19 08:48:00 -0600

    Log errors to database instead of to console

--- src/configsToTasks.js
+++ src/configsToTasks.js
@@ -1,6 +1,17 @@
 var sync = require('places-sync');
 
-module.exports = function (configs, connection, updateSql) {
+var syncWrap = function (config, connection, updateSql, updateErrorSql) {
+  console.log(updateSql);
+  return sync(config.master, config.source, config.destination, config.options).then(function () {
+    return connection.query(updateSql, config);
+  }).catch(function (e) {
+    config.errorMessage = e.message;
+    console.log('Error with ' + config.processName + '\n' + e.message);
+    return connection.query(updateErrorSql, config);
+  });
+};
+
+module.exports = function (configs, connection, updateSql, updateErrorSql) {
   var returnArray = [];
   configs.forEach(function (config) {
     returnArray.push({
@@ -12,15 +23,21 @@ module.exports = function (configs, connection, updateSql) {
     returnArray.push({
       'name': 'Sync ' + config.processName,
       'description': 'Syncing source: ' + config.source.name + ' to ' + config.destination.name,
-      'task': sync,
-      'params': [config.master, config.source, config.destination, config.options]
+      'task': syncWrap,
+      'params': [config, connection, updateSql, updateErrorSql]
     });
-    returnArray.push({
-      'name': 'Update Status ' + config.processName,
-      'description': 'Updates the last_sync field',
-      'task': connection.query,
-      'params': [updateSql, config]
-    });
+  // returnArray.push({
+  //   'name': 'Sync ' + config.processName,
+  //   'description': 'Syncing source: ' + config.source.name + ' to ' + config.destination.name,
+  //   'task': sync,
+  //   'params': [config.master, config.source, config.destination, config.options]
+  // })
+  // returnArray.push({
+  //   'name': 'Update Status ' + config.processName,
+  //   'description': 'Updates the last_sync field',
+  //   'task': connection.query,
+  //   'params': [updateSql, config]
+  // })
   });
   return returnArray;
 };
